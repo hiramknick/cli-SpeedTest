ce99b21c5663673010fee65a20506fdfeda7c075
Hiram Knickerbocker <hiram@mybudget.com.au>
2018-03-17 13:44:25 -0400

    Updates for Header

--- speedtest.py
+++ speedtest.py
@@ -8,11 +8,10 @@ response = subprocess.Popen('speedtest-cli --simple', shell=True, stdout=subproc
 ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
 download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
 upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
-print("TestSuccessfull")
 ##ping[0] = ping[0].replace(',', '.')
 ##download[0] = download[0].replace(',', '.')
 ##upload[0] = upload[0].replace(',', '.')
-if os.stat('/home/pi/results/tests.csv').st_size == 0:
+if os.stat('/home/pi/cli-SpeedTest/results/tests.csv').st_size == 0:
     print('Date,Time,Ping,Download,Upload')
 
 print('{},{},{},{},{}'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping[0], download[0], upload[0]))
